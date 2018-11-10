6cfde84dd632514eaf81d005a0d4ab42de8b3b03
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2018-11-11 01:21:20 +0530

    Added project01

---
+++ project01/lib/main.dart
@@ -0,0 +1,15 @@
+// import library
+import 'package:flutter/material.dart';
+
+// create a main Function
+void main(){
+  // create widget
+
+  var app = MaterialApp(home: Text('Hi there, Vaibhav'));
+
+  // run that widget in mobile
+    runApp(app);
+}
+
+
+
