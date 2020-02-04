abc507d551e88fe23b181ff3595f95cb5fc1ec3e
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-02-04 20:24:11 +0530

    Added packages and setup files

---
+++ Misc Projects/devfest_demo/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:devfesr_demo/utils/devfest.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+  Devfest.prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google DevFest',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
