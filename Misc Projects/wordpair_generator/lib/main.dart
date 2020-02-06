fd5f67fa1b3d06087ef55dfd1c6b91963d5c6e02
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-02-06 19:52:58 +0530

    Added random wordpairs list view

--- Misc Projects/wordpair_generator/lib/main.dart
+++ Misc Projects/wordpair_generator/lib/main.dart
@@ -6,17 +6,54 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.purple[900]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('WordPair Generator'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          // Calculate the number of wordpairs in the list view minus the divider widget
+          final index = item ~/ 2;
+
+          // Generate 10 new wordpairs if we reach the end
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WordPair Generator'),
+      ),
+      body: _buildList(),
     );
   }
 }
