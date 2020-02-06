46f3fdbe5b53f055c2db0fa2268eae762a96661b
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2020-02-06 20:09:44 +0530

    Added favourite functionality

--- Misc Projects/wordpair_generator/lib/main.dart
+++ Misc Projects/wordpair_generator/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+
+import 'random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,51 +10,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return Divider();
-
-          // Calculate the number of wordpairs in the list view minus the divider widget
-          final index = item ~/ 2;
-
-          // Generate 10 new wordpairs if we reach the end
-          if (index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_randomWordPairs[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(fontSize: 18.0),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WordPair Generator'),
-      ),
-      body: _buildList(),
-    );
   }
 }
---
+++ Misc Projects/wordpair_generator/lib/random_words.dart
@@ -0,0 +1,65 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  // A Set can have the same object at once, it can't repeat
+  final _savedWordPairs = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          // Calculate the number of wordpairs in the list view minus the divider widget
+          final index = item ~/ 2;
+
+          // Generate 10 new wordpairs if we reach the end
+          if (index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_randomWordPairs[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(pair);
+          } else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WordPair Generator'),
+      ),
+      body: _buildList(),
+    );
+  }
+}
